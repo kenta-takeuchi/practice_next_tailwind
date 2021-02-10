8106568f1bb70d22e028b94b5faa989225364f62
kenta-takeuchi <49678973+kenta-takeuchi@users.noreply.github.com>
2021-02-11 02:16:43 +0900

    create contact page

--- lesson_next/pages/contact_page.js
+++ lesson_next/pages/contact_page.js
@@ -1,8 +1,41 @@
+import Layout from "../components/Layout";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faUserCircle} from "@fortawesome/free-regular-svg-icons";
+import {faGithub} from "@fortawesome/free-brands-svg-icons";
+import {faFacebook} from "@fortawesome/free-brands-svg-icons";
+import {faTwitter} from "@fortawesome/free-brands-svg-icons";
+
 const Contact = () => {
     return (
-        <div>
-            Contact Page
-        </div>
+        <Layout title="Contact">
+            <div className="bg-white text-center shadow-xl p-8 w-80 rounded">
+                <div className="mt-4">
+                    <p className="font-bold">Contact info</p>
+                </div>
+                <div className="flex justify-center mt-4">
+                    <FontAwesomeIcon icon={faUserCircle} size="5x"/>
+                </div>
+                <div className="mt-4">
+                    <p className="font-bold">Address</p>
+                    <p className="text-xs mt-2 text-gray-600">city A</p>
+                    <p className="font-bold mt-5">Email</p>
+                    <p className="text-xs mt-2 text-gray-600">abc@text.com</p>
+                    <p className="font-bold mt-5">Phone</p>
+                    <p className="text-xs mt-2 text-gray-600">000-1234-5678</p>
+                </div>
+                <div className="mt-6 flex justify-around">
+                    <div>
+                        <FontAwesomeIcon icon={faGithub} size="3x"/>
+                    </div>
+                    <div>
+                        <FontAwesomeIcon icon={faFacebook} size="3x"/>
+                    </div>
+                    <div>
+                        <FontAwesomeIcon icon={faTwitter} size="3x"/>
+                    </div>
+                </div>
+            </div>
+        </Layout>
     )
 }
 
--- lesson_next/pages/index.js
+++ lesson_next/pages/index.js
@@ -4,17 +4,6 @@ export default function Home() {
     return (
         <Layout title="Home">
             <p className="text-4xl">Hello Next.js</p>
-
-            {/*<footer className={styles.footer}>*/}
-            {/*  <a*/}
-            {/*    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"*/}
-            {/*    target="_blank"*/}
-            {/*    rel="noopener noreferrer"*/}
-            {/*  >*/}
-            {/*    Powered by{' '}*/}
-            {/*    <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />*/}
-            {/*  </a>*/}
-            {/*</footer>*/}
         </Layout>
     )
 }
