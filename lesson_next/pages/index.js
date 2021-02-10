c28eb2ef921f3dacf75f3cddd5dd07564b6c8468
kenta-takeuchi <49678973+kenta-takeuchi@users.noreply.github.com>
2021-02-11 01:40:29 +0900

    create next.js app

---
+++ lesson_next/pages/index.js
@@ -0,0 +1,20 @@
+import Layout from "../components/Layout";
+
+export default function Home() {
+    return (
+        <Layout title="Home">
+            <p className="text-4xl">Hello Next.js</p>
+
+            {/*<footer className={styles.footer}>*/}
+            {/*  <a*/}
+            {/*    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"*/}
+            {/*    target="_blank"*/}
+            {/*    rel="noopener noreferrer"*/}
+            {/*  >*/}
+            {/*    Powered by{' '}*/}
+            {/*    <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />*/}
+            {/*  </a>*/}
+            {/*</footer>*/}
+        </Layout>
+    )
+}
